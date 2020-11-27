665254d02a01d30bdee3abd271e7c670dd00e245
hoangminh122 <hoangminh12297@gmail.com>
2020-11-27 17:05:32 +0700

    mario chet animation

--- 05-ScenceManager/Mario.cpp
+++ 05-ScenceManager/Mario.cpp
@@ -967,7 +967,7 @@ void CMario::Render()
 	int alpha = 255;
 	if (untouchable) alpha = 128;
 
-	animation_set->at(90)->Render(x, y, alpha);
+	animation_set->at(ani)->Render(x, y, alpha);
 
 	RenderBoundingBox();
 }
