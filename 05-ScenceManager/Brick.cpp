9adf2903add27d6b5782b9a76f19901cd29766c6
hoangminh122 <hoangminh12297@gmail.com>
2020-12-25 22:39:43 +0700

    tao tuong map

--- 05-ScenceManager/Brick.cpp
+++ 05-ScenceManager/Brick.cpp
@@ -66,6 +66,16 @@ void CBrick::GetBoundingBox(float &l, float &t, float &r, float &b)
 		r = x + BRICK_BBOX_WIDTH;
 		b = y + BRICK_BBOX_HEIGHT_TWO;
 	}
+	else if (type == 13)
+	{
+		r = x + BRICK_BBOX_WIDTH;
+		b = y + BRICK_BBOX_HEIGHT_BORDER;
+	}
+	else if (type == 14)
+	{
+		r = x + BRICK_BBOX_WIDTH_BORDER;
+		b = y + BRICK_BBOX_HEIGHT;
+	}
 
 	
 }
